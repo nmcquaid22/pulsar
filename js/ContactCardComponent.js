1cbcc493809c8d3f344f0e248eecdcd3c93386dd
Nikos Vasileiadis <vasileiadis.nikos@gmail.com>
2017-02-03 10:29:28 +0000

    Add Missing Semicolon to Contact Card JS File

--- js/ContactCardComponent.js
+++ js/ContactCardComponent.js
@@ -13,7 +13,7 @@ ContactCardComponent.prototype.init = function() {
 
     component.$navMain = this.$html.find('.nav-main');
     component.$whosonlineCloseLink = component.$navMain.find('[data-nav-action=whosonline-close]');
-    component.$contactCard = this.$html.find('.contact-card')
+    component.$contactCard = this.$html.find('.contact-card');
     component.$contactCardUserName = this.$contactCard.find('.user-name');
     component.$contactCardJobTitle = this.$contactCard.find('.job-title');
     component.$contactCardAvatarCaption = this.$contactCard.find('.avatar__caption');
