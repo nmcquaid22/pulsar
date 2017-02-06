5bf9bb62026cb99ad3a09cef4e5f99bf1a1af6f3
Nikos Vasileiadis <vasileiadis.nikos@gmail.com>
2017-02-06 09:45:54 +0000

    Change Who’s Online Twig Template to Test Long Texts
    This commit takes very long text entries for the user’s name and job and it trims them after a specific number of characters.

--- js/ContactCardComponent.js
+++ js/ContactCardComponent.js
@@ -6,21 +6,22 @@ function ContactCardComponent(html) {
     this.$html = html;
 }
 
-/* Length of User name Detection */
+/* Length of User's Name Detection */
 ContactCardComponent.prototype.init = function() {
 
     var component = this;
 
-    component.$navMain = this.$html.find('.nav-main');
-    component.$whosonlineCloseLink = component.$navMain.find('[data-nav-action=whosonline-close]');
-    component.$contactCard = this.$html.find('.contact-card')
+    component.$contactCard = this.$html.find('.contact-card');
     component.$contactCardUserName = this.$contactCard.find('.user-name');
     component.$contactCardJobTitle = this.$contactCard.find('.job-title');
-    component.$contactCardAvatarCaption = this.$contactCard.find('.avatar__caption');
 
     component.$contactCardUserName.each(function() {
         if ($(this).html().length > 24) {
             $(this).addClass('long-text');
+        }
+        if ($(this).html().length > 56) {
+            var $trimmedText = $(this).html().slice(0, 56).concat('...');
+            $(this).html($trimmedText);
         }
     });
 
@@ -28,14 +29,11 @@ ContactCardComponent.prototype.init = function() {
         if ($(this).html().length > 24) {
             $(this).addClass('long-text');
         }
+        if ($(this).html().length > 29) {
+            var $trimmedText = $(this).html().slice(0, 29).concat('...');
+            $(this).html($trimmedText);
+        }
     });
-
-    /*
-    1. Name, Surname both long
-    2. One Name, Multiple Surnames
-    3. Multiple Names, Multiple Surnames
-    */
-
 };
 
 module.exports = ContactCardComponent;
